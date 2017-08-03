a9a6139e285aed7f242c8c3ad396d4c81caaf3a2
Brian <bmascitello@gmail.com>
2017-08-03 14:53:48 -0700

    Stopping at "15. Processors"

---
+++ Udacity-Intro-To-Computer-Science/Quizzes/Speed of Light.py
@@ -0,0 +1,12 @@
+# Write Python code to print out how far light travels
+# in centimeters in one nanosecond.  Use the values
+# defined below.
+# speed_of_light = 299792458   meters per second
+# centimeters = 100            one meter is 100 centimeters
+# nanosecond = 1.0/1000000000  one billionth of a second
+
+# Converted to Python 3 syntax
+
+print(299792458 * 100 * 1.0/1000000000)
+
+# 29.9792458 cm / ns
