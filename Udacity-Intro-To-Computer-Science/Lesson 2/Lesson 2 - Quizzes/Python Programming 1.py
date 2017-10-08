850ac6f89b029db966a964dd2172018d3d5615ea
Brian <bmascitello@gmail.com>
2017-10-07 17:37:27 -0700

    Finished Lesson 2

---
+++ Udacity-Intro-To-Computer-Science/Lesson 2/Lesson 2 - Quizzes/Python Programming 1.py
@@ -0,0 +1,7 @@
+# Write Python code that prints out the number of hours in 7 weeks.
+
+hours_in_day = 24
+days_in_week = 7
+number_of_weeks = 7
+
+print(hours_in_day * days_in_week * number_of_weeks)
